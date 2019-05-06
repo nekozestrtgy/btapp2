42e39f08bd27dfd10a572ad858f6f424c9baeba6
nekozestrtgy <nekozestrtgy@gmail.com>
2019-05-06 22:26:08 +0900

    Add cookie-session module

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var session = require('express-session');
+var session = require('cookie-session');
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
@@ -17,15 +17,31 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+var expiryDate = new Date(Date.now() + 180 * 24 * 60 * 60 * 1000)//180日
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
+  name: 'user_session',
+  keys: ['key1', 'key2'],
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    domain: 'localhost',
+    expires: expiryDate
+  }
 }));
+
+var sessionCheck = function (req, res, next) {
+  if (req.session.user_id) {
+    console.log(req.session.user_id)
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
-app.use('/users', usersRouter);
+app.use('/users', sessionCheck, usersRouter);
 
 app.use(function (req, res, next) {
   next(createError(404));
