c27b5e7a76624520d5f4563932a502c5b0ac4962
nekozestrtgy <nekozestrtgy@gmail.com>
2019-05-07 00:21:29 +0900

    Add chat-room function

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.render('users');
+  if (req.session.user_id < 5) {
+    res.render('users', { room: 'first' });
+  } else {
+    res.render('users', { room: 'second' });
+  }
 });
 
 module.exports = router;
